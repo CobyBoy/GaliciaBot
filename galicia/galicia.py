91b6b7c3d1bbef7c63a68cb3088fe39c7a4ca75a
CobyBoy <aboutacoby@gmail.com>
2022-12-07 14:21:54 -0300

    return modificado para evitar que siga loopeando

--- galicia/galicia.py
+++ galicia/galicia.py
@@ -48,7 +48,7 @@ class Galicia(webdriver.Chrome):
                     print("Sueldo depositado")
                     message = const.SUCCESS_MESSAGE + row.text.replace("\n", " ")
                     depositado = True
-        return message
+                    return message
 
     def __exit__(self):
         print("Exiting...")
